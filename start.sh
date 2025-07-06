93f3c9681a3485ab6b9e29dea509894e1b46f0b2
Davi Eduardo <davieduardocostadf@outlook.com>
2025-07-06 19:04:14 -0300

    change something

--- start.sh
+++ start.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if command -v fastfetch >/dev/null 2>&1; then
-  echo "lolcat is fastfetch"
-else
-  echo "fastfetch is NOT installed. Installing..."
-    sudo dnf install fastfetch   
-fi
-
 echo 'Starting script...'
 echo 'copy my file to home' 
+
+# Arch instalation
+if command -v pacman >/dev/null 2>&1; then
+   echo 'Prociding with the arch installation'
+
+   if command -v fastfetch >/dev/null 2>&1; then
+      echo "lolcat is fastfetch"
+   else
+      echo "fastfetch is NOT installed. Installing..."
+      yay -S fastfetch   
+   fi
+elif command -v dnf >/dev/null 2>&1; then
+    echo 'its using dnf'
+fi
+
+## commands
 cp $HOME/configs/bash-config/bash.config $HOME/
 echo 'backup the currently bashrc file'
 mv $HOME/.bashrc $HOME/.bashrc.back
